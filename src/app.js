eca7112beaa2b7feb1181fa2004fd1e1d94b9b05
Josh Bedo <joshbedo@yahoo.com>
2015-01-10 16:12:24 -0500

    Fixing merge conflicts

--- src/app.js
+++ src/app.js
@@ -4,17 +4,6 @@ var App = {
   Collections: {},
   Views: {},
 
-<<<<<<< Updated upstream
-	init: function() {
-		this.appView = new this.Views.AppView({ el: '#app' });
-		this.users = new this.Collections.Users();
-	},
-	start: function() {
-		Backbone.history.start();
-		var usersView = new this.Views.UsersView({ collection: this.users });
-		this.appView.show(usersView);
-	}
-=======
   init: function() {
     this.appView = new this.Views.AppView({ el: '#app' });
     this.users = new this.Collections.Users();
@@ -24,30 +13,10 @@ var App = {
     var usersView = new this.Views.UsersView({ collection: this.users });
     this.appView.show(usersView);
   }
->>>>>>> Stashed changes
 };
 
 // Models
 App.Models.User = Backbone.Model.extend({
-<<<<<<< Updated upstream
-	url: '/api/users',
-	defaults: {
-		firstName: 'Josh',
-		lastName: 'Bedo'
-	},
-
-	initialize: function() {
-		this.on('invalid', function(m) {
-			alert(m.validationError);
-		});
-	},
-
-	validate: function(attrs, opts) {
-		if (!(attrs.email && attrs.userName)) {
-			return "Need an email AND a username";
-		}
-	}
-=======
   url: '/api/users',
   defaults: {
     firstName: 'Josh',
@@ -70,20 +39,10 @@ App.Models.User = Backbone.Model.extend({
     return this.get('firstName') + ' ' + this.get('lastName');
   }
 
->>>>>>> Stashed changes
 });
 
 // Collections
 App.Collections.Users = Backbone.Collection.extend({
-<<<<<<< Updated upstream
-	url: '/api/users',
-	initialize: function() {
-		this.on('change', onChange);
-	},
-	onChange: function(model) {
-		console.info('Model in App.Collections.Users has changed', model);
-	}
-=======
   url: '/api/users',
   initialize: function() {
     this.on('change', this.onChange);
@@ -91,41 +50,17 @@ App.Collections.Users = Backbone.Collection.extend({
   onChange: function(model) {
     console.info('Model in App.Collections.Users has changed', model);
   }
->>>>>>> Stashed changes
 });
 
 // View for the app container
 App.Views.AppView = Backbone.View.extend({
-<<<<<<< Updated upstream
-	show: function(view) {
-		this.$el.append(view.el);
-	}
-=======
   show: function(view) {
     this.$el.append(view.el);
   }
->>>>>>> Stashed changes
 });
 
 // Users CollectionView
 App.Views.UsersView = Backbone.View.extend({
-<<<<<<< Updated upstream
-	tagName: 'ul',
-	initialize: function() {
-		this.collection.on('sync', this.render, this);
-	},
-	render: function() {
-		var el = this.$el;
-		el.empty();
-
-		this.collection.each(function(item) {
-			var userView = new UserView({ model: item });
-			var rendered = userView.render();
-			el.append(rendered);
-		});
-		return el;
-	}
-=======
   tagName: 'ul',
   initialize: function() {
     this.collection.on('sync', this.render, this);
@@ -141,22 +76,13 @@ App.Views.UsersView = Backbone.View.extend({
     });
     return el;
   }
->>>>>>> Stashed changes
 });
 
 // Users ItemView
 App.Views.UserView = Backbone.View.extend({
-<<<<<<< Updated upstream
-	tagName: 'li',
-	render: function() {
-		this.$el.append(this.model.get('email'));
-		return this.$el;
-	}
-=======
   tagName: 'li',
   render: function() {
     this.$el.append(this.model.get('email'));
     return this.$el;
   }
->>>>>>> Stashed changes
 });
