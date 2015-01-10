6887051939a53c9694143aa0cb2f4f30583ad75d
Josh Bedo <joshbedo@yahoo.com>
2015-01-10 16:05:33 -0500

    Updating tests

--- test/unit/collection.spec.js
+++ test/unit/collection.spec.js
@@ -1,11 +1,22 @@
 describe('App.Collections.User', function() {
+  beforeEach(function() {
+    this.collection = new App.Collections.Users([
+      { firstName: 'Josh', lastName: 'Bedo' }
+    ]);
+  });
+
   it('should have a populated collection', function() {
-    expect(false).to.be(true);
+    expect(this.collection.length).to.equal(3);
   });
 
   it('should fire change event when model is changed', function() {
-    expect(true).to.be(false);
+    var spy = sinon.spy();
+    this.collection.on('change', spy);
+
+    var user = this.collection.first();
+    user.set('firstName', 'James');
+
+    expect(spy).to.be.called;
   });
 
 });
-
--- test/unit/model.spec.js
+++ test/unit/model.spec.js
@@ -1,13 +1,30 @@
 describe('App.Models.User', function() {
+
+  beforeEach(function() {
+    this.model = new App.Models.User();
+  });
+
   it('should have default values', function() {
-    expect(true).to.be(false);
+    expect(this.model.get('firstName')).is.equal('Josh');
+    expect(this.model.get('lastName')).is.equal('Bedo');
   });
 
   it('should validate user model', function() {
-    expect(true).to.be(false);
+    this.model.save();
+    expect(this.model.validationError).to.be.defined;
+    expect(this.model.validationError).to.equal('Needs a username and email');
   });
 
   it('should call the #fullName method', function() {
-    expect(true).to.be(false);
+    sinon.spy(this.model, 'fullName');
+    var fullName = this.model.fullName(),
+        firstName = fullName.split(" ")[0],
+        lastName = fullName.split(" ")[1];
+
+    expect(this.model.fullName).to.be.defined;
+    expect(this.model.fullName).to.be.calledOnce;
+    expect(this.model.get('firstName')).to.equal('Josh');
+    expect(this.model.get('lastName')).to.equal('Bedo');
   });
+
 });
