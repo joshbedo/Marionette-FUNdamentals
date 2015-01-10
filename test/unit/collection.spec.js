6deee514683ef5a6cad727578bc5bd3294681d60
Josh Bedo <joshbedo@yahoo.com>
2015-01-10 15:45:29 -0500

    Adding skeleton for tests

--- test/unit/collection.spec.js
+++ test/unit/collection.spec.js
@@ -1,2 +1,11 @@
-describe('App.Collections.User', function() {});
+describe('App.Collections.User', function() {
+  it('should have a populated collection', function() {
+    expect(false).to.be(true);
+  });
+
+  it('should fire change event when model is changed', function() {
+    expect(true).to.be(false);
+  });
+
+});
 
--- test/unit/model.spec.js
+++ test/unit/model.spec.js
@@ -1,1 +1,13 @@
-describe('App.Models.User', function() {});
+describe('App.Models.User', function() {
+  it('should have default values', function() {
+    expect(true).to.be(false);
+  });
+
+  it('should validate user model', function() {
+    expect(true).to.be(false);
+  });
+
+  it('should call the #fullName method', function() {
+    expect(true).to.be(false);
+  });
+});
