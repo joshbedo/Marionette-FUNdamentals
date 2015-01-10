d72be80369c534e0bd88284c424b8fd9c07b44a5
Josh Bedo <joshbedo@yahoo.com>
2015-01-10 12:29:55 -0500

    Adding Marionette

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
 // App namespace
 
-var App = new Backbone.Marionette.Application.extend({
+var App = Marionette.Application.extend({
 
 	Models: {},
 	Collections: {},
-	Views: {},
-
-	init: function() {
-		this.appView = new this.Views.AppView({ el: '#app' });
-		this.users = new this.Collections.Users();
-	},
-	start: function() {
-		Backbone.history.start();
-		var usersView = new this.Views.UsersView({collection: this.users});
-		this.appView.show(usersView);
-	}
+	Views: {}
+	// init: function() {
+	// 	this.appView = new this.Views.AppView({ el: '#app' });
+	// 	this.users = new this.Collections.Users();
+	// },
+	// start: function() {
+	// 	Backbone.history.start();
+	// 	var usersView = new this.Views.UsersView({collection: this.users});
+	// 	this.appView.show(usersView);
+	// }
 
 });
 
+MyApp = new App();
+
 // Models
-App.Models.User = Backbone.Model.extend({
+MyApp.Models.User = Backbone.Model.extend({
 	url: '/api/users',
 	defaults: {
 		firstName: 'Josh',
@@ -44,44 +45,36 @@ App.Models.User = Backbone.Model.extend({
 });
 
 // Collections
-App.Collections.Users = Backbone.Collection.extend({
+MyApp.Collections.Users = Backbone.Collection.extend({
 	url: '/api/users'
 });
 
 // View for the app container
-App.Views.AppView = Backbone.View.extend({
+MyApp.Views.AppView = Backbone.View.extend({
 	show: function(view) {
 		this.$el.append(view.el);
 	}
 });
 
-// Users CollectionView
-App.Views.UsersView = Backbone.View.extend({
-	tagName: 'ul',
-	initialize: function() {
-		this.collection.on('sync', this.render, this);
-	},
-	render: function() {
-		var el = this.$el;
-		el.empty();
-
-		this.collection.each(function(item) {
-			var userView = new UserView({ model: item });
-			var rendered = userView.render();
-			el.append(rendered);
-		});
-		return el;
-	}
-});
 
 // Users ItemView
-App.Views.UserView = Backbone.View.extend({
-	tagName: 'li',
-	render: function() {
-		this.$el.append(this.model.get('email'));
-		return this.$el;
+MyApp.Views.UserView = Marionette.ItemView.extend({
+	template: _.template('testing'),
+	tagName: 'li'
+});
+
+// Users CollectionView
+MyApp.Views.UsersView = Marionette.CollectionView.extend({
+	tagName: 'ul',
+	childView: MyApp.Views.UserView,
+	initialize: function() {
+		this.collection.on('sync', this.render, this);
 	}
 });
+
+// MyApp.start();
+
+Backbone.history.start();
 
 //App.addRegions({
 //	main: "#main",
