77538e3c2132184e38f73cd3a4fe73085d8a9939
Josh Bedo <joshbedo@yahoo.com>
2015-01-10 13:52:29 -0500

    Adding test for change event on collection

--- test/unit/collection.spec.js
+++ test/unit/collection.spec.js
@@ -11,12 +11,22 @@ describe('App.Collections.User', function() {
     expect(this.collection.length).to.equal(3);
   });
 
+  // it('should return model when #findWhere is called', function() {
+  //   var model = new App.Models.User();
+  //   sinon.stub(this.collection, 'findWhere').returns(model)
+
+  //   var user = this.collection.findWhere();
+  //   expect(user).to.equal(model);
+  // });
+
   it('should fire change event when model is changed', function() {
-    var spy = sinon.spy(this.collection.prototype, 'onChange');
+    var spy = sinon.spy();
+    this.collection.on('change', spy);
+
     var user = this.collection.first();
     user.set('firstName', 'James');
 
-    expect(spy).to.be.calledOnce;
+    expect(spy).to.be.called;
   });
 
 });
--- test/unit/model.spec.js
+++ test/unit/model.spec.js
@@ -14,4 +14,16 @@ describe('App.Models.User', function() {
     expect(this.model.validationError).to.be.defined;
   });
 
+  it('should call the #fullName method', function() {
+    sinon.spy(this.model, 'fullName');
+    var fullName = this.model.fullName(),
+        firstName = fullName.split(" ")[0],
+        lastName = fullName.split(" ")[1];
+
+    expect(this.model.fullName).to.be.defined;
+    expect(this.model.fullName).to.be.calledOnce;
+    expect(this.model.get('firstName')).to.equal('Josh');
+    expect(this.model.get('lastName')).to.equal('Bedo');
+  });
+
 });
