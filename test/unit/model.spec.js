b8fcf34e979a89091a46ec557705f187e965f6e8
Josh Bedo <joshbedo@yahoo.com>
2015-01-10 10:33:33 -0500

    Updating model spec and making sure i switched over from Joanne's git account LOL

--- test/unit/model.spec.js
+++ test/unit/model.spec.js
@@ -1,8 +1,20 @@
 describe('App.Models.User', function() {
-  it('should have default values', function() {
-    var model = new App.Models.User();
 
-    expect(model.get('firstName')).is.equal('Josh');
-    expect(model.get('lastName')).is.equal('Bedo');
+  beforeEach(function() {
+    this.model = new App.Models.User();
   });
+
+  it('should have default values', function() {
+    expect(this.model.get('firstName')).is.equal('Josh');
+    expect(this.model.get('lastName')).is.equal('Bedo');
+  });
+
+  it('should validate user model', function() {
+    this.model.save();
+    expect(this.model.validationError).to.be.defined;
+  });
+
 });
+
+// describe('App.Collections.User', function() {
+// });
